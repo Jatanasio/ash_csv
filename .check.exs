917eaa401d20b26a8153bfb878c9a66f92eadefa
James Harton <james@harton.nz>
2025-10-15 09:04:48 +1300

    chore: Fix REUSE copyright attributions

--- .check.exs
+++ .check.exs
@@ -1,4 +1,4 @@
-# SPDX-FileCopyrightText: 2020 Zach Daniel
+# SPDX-FileCopyrightText: 2020 ash_csv contributors <https://github.com/ash-project/ash_csv/graphs.contributors>
 #
 # SPDX-License-Identifier: MIT
 
